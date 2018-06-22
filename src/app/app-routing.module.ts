d05417fb8020a714f473d7b64540dd2208e7b5c8
Luan Sousa <codeit10@fornecedor.gamasaude.com.br>
2018-06-22 11:33:13 -0300

    Adicionada a primeira tela

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { InicialComponent } from './telas/inicial/inicial.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  { path: '', component: InicialComponent },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicialComponent } from './telas/inicial/inicial.component';
+import { SideNavComponent } from './layouts/side-nav/side-nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicialComponent,
+    SideNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
