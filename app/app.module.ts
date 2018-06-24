3593f8162893dad0108c7de3da428ecfe44942a4
luansousa26 <luan2850@gmail.com>
2018-06-24 12:39:35 -0300

    Auto-generated commit

---
+++ app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicialComponent } from './telas/inicial/inicial.component';
+import { SideNavComponent } from './layouts/side-nav/side-nav.component';
+import { PrincipalComponent } from './telas/principal/principal.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicialComponent,
+    SideNavComponent,
+    PrincipalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
