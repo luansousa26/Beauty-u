e917604fe51a8db02e69b7a1ffb83a2dac5169a7
luansousa26 <luan2850@gmail.com>
2018-07-06 23:36:26 -0300

    Adicionado a rota de serviços

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SobreComponent } from './telas/sobre/sobre.component';
+import { ServicosComponent } from './telas/servicos/servicos.component';
 const routes: Routes = [
   { path: '', component: InicialComponent },
   { path: 'sobre', component: SobreComponent},
+  { path: 'servicos', component: ServicosComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
