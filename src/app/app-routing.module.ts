b05023763f146dc82682fc067fcae2445ff7c549
luansousa26 <luan2850@gmail.com>
2018-07-06 23:40:17 -0300

    Adicionada a rota de fale conosco

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SobreComponent } from './telas/sobre/sobre.component';
 import { ServicosComponent } from './telas/servicos/servicos.component';
+import { FaleconoscoComponent } from './telas/faleconosco/faleconosco.component';
 const routes: Routes = [
   { path: '', component: InicialComponent },
   { path: 'sobre', component: SobreComponent},
-  { path: 'servicos', component: ServicosComponent}
+  { path: 'servicos', component: ServicosComponent},
+  { path: 'fale-conosco', component: FaleconoscoComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
