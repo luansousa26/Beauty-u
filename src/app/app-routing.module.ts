b55100123847ac955d6c69fecd3449b63e22cfd0
luansousa26 <luan2850@gmail.com>
2018-07-06 23:33:55 -0300

    Adicionado a rota de sobre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { InicialComponent } from './telas/inicial/inicial.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { SobreComponent } from './telas/sobre/sobre.component';
 const routes: Routes = [
   { path: '', component: InicialComponent },
+  { path: 'sobre', component: SobreComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
