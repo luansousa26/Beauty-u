d0e39226eb0542b333edbdd6f9a9f0e054d737f3
Luan Sousa <luan26@uninove.edu.br>
2018-07-16 00:50:03 -0300

    Imagens alterando

--- src/app/telas/inicial/inicial.component.ts
+++ src/app/telas/inicial/inicial.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicial.component.scss']
 })
 export class InicialComponent implements OnInit {
-
+  imagensDisponiveis: any[] = [];
+  controlador = -1;
   constructor() { }
 
   ngOnInit() {
+    this.imagensDisponiveis = [
+      '../../../assets/background.jpg',
+      '../../../assets/background2.jpg',
+      '../../../assets/background3.jpg',
+      '../../../assets/background4.jpg'
+    ];
+    this.startTimer();
+  }
+  displayNextImage() {
+    this.controlador = (this.controlador === this.imagensDisponiveis.length - 1) ? 0 : this.controlador + 1;
+    document.getElementById('img').style.backgroundImage = `url('${this.imagensDisponiveis[this.controlador]}')`;
   }
 
+  displayPreviousImage() {
+    this.controlador = (this.controlador <= 0) ? this.imagensDisponiveis.length - 1 : this.controlador - 1;
+    document.getElementById("img").style.backgroundImage  = `url('${this.imagensDisponiveis[this.controlador]}')`;
+  }
+
+  startTimer() {
+    setInterval(() => {
+      this.displayNextImage();
+    }, 5000);
+  }
 }
