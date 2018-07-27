b50c17c3845ed918a60c61021bafcf93f9bd7c31
Luan Sousa <luan26@uninove.edu.br>
2018-07-27 12:41:03 -0300

    coordenadas atualizadas

--- src/app/telas/sobre/sobre.component.ts
+++ src/app/telas/sobre/sobre.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobre.component.scss']
 })
 export class SobreComponent implements OnInit {
-  
-  lat = -23.2828262;
-  lng = -46.7455076;
+
+  lat = -23.28466259;
+  lng = -46.7380071;
   constructor() { }
 
   ngOnInit() {
